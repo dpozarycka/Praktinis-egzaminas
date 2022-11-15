f05baed97225f1249b5c99ca9b2c1d748938d805
dpozarycka <dianapozarycka@gmail.com>
2022-11-15 17:30:43 +0200

    init

--- script.js
+++ script.js
@@ -33,3 +33,63 @@ const swiper = new Swiper('.swiper', {
     }
 });
 
+const burger = document.getElementById("burger")
+const sideNav = document.querySelector(".side-nav")
+
+burger.addEventListener("click", function(){
+    this.classList.toggle("rotate")
+    sideNav.classList.toggle("translate")
+    sideNavOpened = true
+})
+
+window.addEventListener("resize", function(){
+    if(this.window.innerWidth > 768){
+        burger.classList.remove("rotate")
+        sideNav.classList.remove("translate")
+    }
+})
+
+// Scroll link animation
+
+const nav1 = document.querySelectorAll("nav")[0]
+const links1 = nav1.querySelectorAll(".link")
+
+const nav2 = document.querySelectorAll("nav")[1]
+const links2 = nav2.querySelectorAll(".link")
+
+const sections = document.querySelectorAll("section")
+window.addEventListener("scroll", function(){
+    let current = ""
+    for(let section of sections){
+        const sectionTop = section.offsetTop
+        if(window.pageYOffset >= sectionTop - 50){
+            current = section.getAttribute("id")
+        }
+    }    
+
+    for (var x of links1){
+        x.classList.remove("active")
+        if(x.getAttribute("href") === "#" + current){
+            x.classList.add("active")
+        }
+    }
+
+    for (var x of links2){
+        x.classList.remove("active")
+        if(x.getAttribute("href") === "#" + current){
+            x.classList.add("active")
+        }
+    }
+})
+
+document.querySelector("h2").classList.add("visible")
+
+// Close side nav
+let sideNavOpened = false
+for (let link of links2){
+    link.addEventListener("click", function(){
+        burger.classList.remove("rotate")
+        sideNav.classList.remove("translate")
+        sideNavOpened = false
+    })
+}

e922c13a9b06a8b0f342ac5474adb30df45278c3
dpozarycka <dianapozarycka@gmail.com>
2022-11-15 14:59:36 +0200

    init

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  allowSlideNext: true,
+  allowSlidePrev: true,
+  speed: 1000,
+  autoplay: {
+      disableOnInteraction: false,
+      pauseOnMouseEnter: true,
+      delay: 2000
+  },
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  breakpoints: {
+      1000: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        allowSlideNext: false,
+        allowSlidePrev: false,
+        autoplay: false
+      }
+    }
+});
+
